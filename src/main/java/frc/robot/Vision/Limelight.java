130eef38ddd2eedb473fcb4bada363594c58c6fb
spybh66 <brysonh99@gmail.com>
2024-03-08 16:51:01 -0500

    Initial commit of limelight based note targeting

--- src/main/java/frc/robot/Vision/Limelight.java
+++ src/main/java/frc/robot/Vision/Limelight.java
@@ -15,70 +15,43 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Subsystems.Drivetrain.CommandSwerveDrivetrain;
 import frc.robot.Util.RectanglePoseArea;
+import static frc.robot.Constants.LimelightConstants.*;
 
 public class Limelight extends SubsystemBase {
-    CommandSwerveDrivetrain drivetrain;
+    CommandSwerveDrivetrain m_drivetrain;
     Alliance alliance;
-    private String ll = "limelight";
+    private String ll = kCameraName;
     private Boolean enable = false;
-    private Boolean trust = false;
-    private int fieldError = 0;
-    private int distanceError = 0;
-    private Pose2d botpose;
-    private static final RectanglePoseArea field = new RectanglePoseArea(new Translation2d(0.0, 0.0), new Translation2d(16.54, 8.02));
+    private double tx;
+    private double ty;
+    private Boolean hasTarget = false;
 
     /** Creates a new Limelight. */
     public Limelight(CommandSwerveDrivetrain drivetrain) {
-        this.drivetrain = drivetrain;
-        SmartDashboard.putNumber("Field Error", fieldError);
-        SmartDashboard.putNumber("Limelight Error", distanceError);
+        m_drivetrain = drivetrain;
+
     }
 
     @Override
     public void periodic() {
-        /* if (DriverStation.isEnabled()) {
-            trust = false;
-        } else {
-            trust = true;
-        }
-
         if (enable) {
-            Double targetDistance = LimelightHelpers.getTargetPose3d_CameraSpace(ll).getTranslation().getDistance(new Translation3d());
-            Double confidence = 1 - ((targetDistance - 1) / 6);
-            LimelightHelpers.Results result = LimelightHelpers.getLatestResults(ll).targetingResults;
-            if (result.valid) {
-                if (alliance == Alliance.Blue) {
-                    botpose = LimelightHelpers.getBotPose2d_wpiBlue(ll);
-                } else if (alliance == Alliance.Red) {
-                    botpose = LimelightHelpers.getBotPose2d_wpiRed(ll);
-                }
-                if (field.isPoseWithinArea(botpose)) {
-                    if (drivetrain.getState().Pose.getTranslation().getDistance(botpose.getTranslation()) < 0.5 || trust
-                            || result.targets_Fiducials.length > 1) {
-                        drivetrain.addVisionMeasurement(botpose,
-                                Timer.getFPGATimestamp() - (result.latency_capture / 1000.0) - (result.latency_pipeline / 1000.0),
-                                VecBuilder.fill(confidence, confidence, .01));
-                    } else {
-                        distanceError++;
-                        SmartDashboard.putNumber("Limelight Error", distanceError);
-                    }
+            if (LimelightHelpers.getTV(kCameraName)) {
+                LimelightHelpers.Results result = LimelightHelpers.getLatestResults(ll).targetingResults;
+                if (result.valid && LimelightHelpers.getNeuralClassID(kCameraName) == 0) {
+                    hasTarget = true;
                 } else {
-                    fieldError++;
-                    SmartDashboard.putNumber("Field Error", fieldError);
+                    hasTarget = false;
                 }
             }
-        } */
-    }
-
-    public void setAlliance(Alliance alliance) {
-        this.alliance = alliance;
+        }
     }
 
     public void useLimelight(boolean enable) {
         this.enable = enable;
     }
 
-    public void trustLL(boolean trust) {
-        this.trust = trust;
+    public boolean hasTarget() {
+        return hasTarget;
     }
+
 }
